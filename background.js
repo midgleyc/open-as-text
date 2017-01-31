942ba2229f1b22fd818613a5c60b6e911ba8a989
Chris Midgley <midgleyc@gmail.com>
2017-01-31 19:30:43 +0000

    Use specific listener instead of global variable.

--- background.js
+++ background.js
@@ -1,34 +1,29 @@
 "use strict";
 
-var isOn = 0;
-
 function openAsText(data) {
-  isOn++;
+  browser.webRequest.onHeadersReceived.addListener(rewriteHeaders,
+                                          {urls: [data.linkUrl]},
+                                          ['blocking', 'responseHeaders']);
+  var removeHeader = () => {browser.webRequest.onHeadersReceived.removeListener(rewriteHeaders)};
   var creating = browser.tabs.create({url: data.linkUrl});
-  creating.then(() => {isOn--}, () => {isOn--})
+  creating.then(removeHeader, removeHeader)
 }
 
 function rewriteHeaders(e) {
-  if (isOn) {
-    var contentType = [];
-    var contentDisposition = [];
-    for (var header of e.responseHeaders) {
-      if (header.name == 'Content-Type') {
-	    contentType = header;
-	  }
-      if (header.name == 'Content-Disposition') {
-	    contentDisposition = header;
-	  }
-    }
-    contentType.value = 'text/plain;charset=UTF-8';
-    contentDisposition.value = '';
+  var contentType = [];
+  var contentDisposition = [];
+  for (var header of e.responseHeaders) {
+    if (header.name == 'Content-Type') {
+	  contentType = header;
+	}
+    if (header.name == 'Content-Disposition') {
+	  contentDisposition = header;
+	}
   }
+  contentType.value = 'text/plain;charset=UTF-8';
+  contentDisposition.value = '';
   return {responseHeaders: e.responseHeaders};
 }
 
 browser.contextMenus.create({id: "text-open", title: "Open as Text", contexts: ["link"]});
 browser.contextMenus.onClicked.addListener(openAsText);
-
-browser.webRequest.onHeadersReceived.addListener(rewriteHeaders,
-                                          {urls: ['<all_urls>']},
-                                          ['blocking', 'responseHeaders']);
