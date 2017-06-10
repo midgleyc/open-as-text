bf33935685c40b8d17da92ccbc4f4c760bcbe4fd
Nikola Kocic <nikolakocic@gmail.com>
2017-06-10 12:02:05 +0200

    Remove onHeadersReceived listener in onResponseStarted.
    Previously it was removed when tab was opened, which is triggered before onHeadersReceived

--- background.js
+++ background.js
@@ -1,17 +1,54 @@
+/* jshint moz: true, esversion: 6 */
+
 "use strict";
 
+var isDebug = false;
+
+if (isDebug) var debug = console.log.bind(window.console);
+else var debug = function(){};
+
+function removeHeaderListener() {
+  debug("onHeadersReceived.removeListener");
+  browser.webRequest.onHeadersReceived.removeListener(rewriteHeaders);
+}
+
 function openAsText(data) {
-  browser.webRequest.onHeadersReceived.addListener(rewriteHeaders,
-                                          {urls: [data.linkUrl]},
-                                          ['blocking', 'responseHeaders']);
-  var removeHeader = () => {browser.webRequest.onHeadersReceived.removeListener(rewriteHeaders)};
-  var creating = browser.tabs.create({url: data.linkUrl});
-  creating.then(removeHeader, removeHeader)
+  debug("openAsText", data);
+  const url = data.linkUrl;
+
+  debug("onHeadersReceived.addListener for URL", url);
+  browser.webRequest.onHeadersReceived.addListener(
+      rewriteHeaders,
+      {urls: [url]},
+      ['blocking', 'responseHeaders']
+  );
+
+  const removeHeaderListenerOnResponse = () => {
+    const listener = () => {
+      removeHeaderListener();
+      // remove self listener
+      debug("onResponseStarted.removeListener");
+      browser.webRequest.onResponseStarted.removeListener(listener);
+    };
+
+    debug("onResponseStarted.addListener for URL", url);
+    browser.webRequest.onResponseStarted.addListener(
+        listener,
+        {urls: [url]}
+    );
+  };
+
+  const creating = browser.tabs.create({url: url});
+  creating.then(
+      removeHeaderListenerOnResponse /* onCreated */,
+      removeHeaderListener /* onError */
+  );
 }
 
 function rewriteHeaders(e) {
-  var contentType = [];
-  var contentDisposition = [];
+  debug("rewriteHeaders", e);
+  let contentType = [];
+  let contentDisposition = [];
   for (var header of e.responseHeaders) {
     if (header.name.toLowerCase() == 'content-type') {
       contentType = header;
